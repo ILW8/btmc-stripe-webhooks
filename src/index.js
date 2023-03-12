e1197e035a922cde4fc08f170da18567264df862
Dao Heng Liu <daohengliu@gmail.com>
2023-03-12 14:42:35 +0000

    add bulk of functional code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,133 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// https://stripe.com/docs/webhooks/signatures#verify-manually
+// const webhook_endpoint_secret = "whsec_4279b220ceec22b94873693786cc2b4e09245da72a600ef264fbaf48c5a8fe74";
 
+let webhook_url = "";  // will be fetched from environment variables
+let default_avatar_url = "https://media.discordapp.net/attachments/261053355793580032/1084349163698929744/meme-boy-gets-paid-4140-196a182847d3123a0e377b1059e07ceb1x.png";
+const webhook_message_template = {
+    "content": "",
+    "embeds": [
+        {
+            "title": "placeholder title",
+            "description": "",
+            "url": "https://theroundtable.gg",
+            "color": 10616664,
+            "fields": [
+                {
+                    "name": "yes",
+                    "value": "1"
+                },
+                {
+                    "name": "wwwww",
+                    "value": "w2"
+                }
+            ],
+            "footer": {
+                "text": "nom nom nom money nom nom"
+            },
+            "timestamp": new Date(parseInt("0") * 1000).toISOString()
+        }
+    ],
+    "username": "roundtable funding secured",
+    "avatar_url": default_avatar_url,
+    "attachments": []
+}
+
+// noinspection JSUnusedLocalSymbols
 export default {
-	async fetch(request, env, ctx) {
-		return new Response("Hello World!");
-	},
+    async fetch(request, env, ctx) {
+        // webhook handler
+        const {pathname} = new URL(request.url);
+        if (pathname !== "/webhook" || request.method !== "POST") {
+            return new Response(null, {status: 404});
+        }
+
+        // ensure required environment variables are present.
+        // set environment variables inside wrangler.toml in this project directory or set them directly on
+        // cloudflare workers page (settings tab on the top, then variables tab on the left)
+        webhook_message_template.avatar_url = env.DISCORD_WEBHOOK_AVATAR_URL ?? default_avatar_url;
+        if (!("DISCORD_WEBHOOK_URL" in env) || env.DISCORD_WEBHOOK_URL.length === 0){
+            return new Response("This worker is misconfigured. Required environment variable(s) are missing.", {status: 500});
+        }
+        webhook_url = env.DISCORD_WEBHOOK_URL
+
+        let event = await request.json();
+        let webhook_message = JSON.parse(JSON.stringify(webhook_message_template)); // deep copy of template
+
+        webhook_message.embeds[0].timestamp = new Date(parseInt(event.created) * 1000).toISOString();
+        webhook_message.embeds[0].title = "Event type: " + event.type;
+        webhook_message.embeds[0].url = "https://dashboard.stripe.com/events/" + event.id;
+
+        switch (event.type) {
+            case 'balance.available':
+                const balanceAvailable = event.data.object;
+                // Then define and call a function to handle the event balance.available
+                webhook_message.embeds[0].fields.length = 0;
+                for (const avail of balanceAvailable.available) {
+                    console.log(avail);
+                    webhook_message.embeds[0].fields.push({
+                        "name": "Available balance (" + avail.currency + ")",
+                        "value": "" + (avail.amount / 100).toFixed(2),
+                    })
+                }
+
+                break;
+            case 'charge.succeeded':
+                const chargeSucceeded = event.data.object;
+                // Then define and call a function to handle the event charge.succeeded
+                webhook_message.embeds[0].url = "https://dashboard.stripe.com/payments/" + chargeSucceeded.id;
+
+                webhook_message.embeds[0].fields.length = 0;
+                webhook_message.embeds[0].fields.push({
+                    "name": "Charge amount",
+                    "value": "" + (chargeSucceeded.amount / 100).toFixed(2) + " " + chargeSucceeded.currency
+                });
+                webhook_message.embeds[0].fields.push({
+                    "name": "Effective amount (-2.9% and -0.30USD)",
+                    "value": "" + (0.971 * (chargeSucceeded.amount - 30) / 100).toFixed(2) + " " + chargeSucceeded.currency
+                });
+                webhook_message.embeds[0].fields.push({
+                    "name": "Donor",
+                    "value": chargeSucceeded.metadata.username ?? "unknown"
+                });
+
+                break;
+            // case 'payment_intent.succeeded':
+            //     const paymentIntentSucceeded = event.data.object;
+            //     // Then define and call a function to handle the event payment_intent.succeeded
+            //     webhook_message.embeds[0].fields.length = 0;
+            //     webhook_message.embeds[0].fields.push({"name": "Charge amount", "value": "" + chargeSucceeded.amount + " " + chargeSucceeded.currency});
+            //     webhook_message.embeds[0].fields.push({"name": "Donor", "value": chargeSucceeded.metadata.username ?? "unknown"});
+            //     break;
+            case 'payout.paid':
+                const payoutPaid = event.data.object;
+                // Then define and call a function to handle the event payout.paid
+                webhook_message.embeds[0].fields.length = 0;
+                webhook_message.embeds[0].fields.push({
+                    "name": "Expected payout arrival date",
+                    "value": "<t:" + payoutPaid.arrival_date + ":F>"
+                });
+                webhook_message.embeds[0].fields.push({
+                    "name": "Payout amount",
+                    "value": ((payoutPaid.amount ?? -100) / 100).toFixed(2)
+                });
+                break;
+            // ... handle other event types
+            default:
+                // webhook_message.embeds.length = 0;
+                // webhook_message.content = "Unknown event type " + event.type + " received, view details in dashboard.";
+                return new Response(null);
+        }
+
+        await fetch(webhook_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            redirect: "follow",
+            body: JSON.stringify(webhook_message),
+        });
+
+
+        return new Response(null);
+    },
 };
